9bca979569502ed4c715a0ff69a18b8e5a6682f6
Alvin Peter <nivlapeter.0549@gmail.com>
2019-03-25 14:23:09 +0300

    push to master

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+ function SUBMIT(){
+    var side1=parseFloat(document.getElementById("side1").value);
+    var side2=parseFloat(document.getElementById("side2").value);
+    var side3=parseFloat(document.getElementById("side3").value);
+    
+    if(side1===side2 && side2===side3 && side1===side3){
+        alert ("Equilateral Triangle");
+    }
+    else if(side1===side2||side2===side3||side1===side3){
+        alert ("Isoseles Triangle");
+    }
+    else if(side1!=side2 && side2!=side3 && side1!=side3){
+        alert ("Scalene Triangle");
+    }
+    else{
+        alert("Not a triangle"); 
+    }
+    
+}
