82c6cda345a53b350d4dffe7219531fb8d6f8034
Alvin Peter <nivlapeter.0549@gmail.com>
2019-03-26 10:00:38 +0300

    declare variables

--- js/scripts.js
+++ js/scripts.js
@@ -3,17 +3,17 @@
     var side2=parseFloat(document.getElementById("side2").value);
     var side3=parseFloat(document.getElementById("side3").value);
   
-    if(side1===side2 && side2===side3 && side1===side3){
-        alert ("Equilateral Triangle");
-    }
-    else if(side1===side2||side2===side3||side1===side3){
-        alert ("Isoseles Triangle");
-    }
-    else if(side1!=side2 && side2!=side3 && side1!=side3){
-        alert ("Scalene Triangle");
-    }
-    else{
-        alert("Not a triangle"); 
-    }
+    // if(side1===side2 && side2===side3 && side1===side3){
+    //     alert ("Equilateral Triangle");
+    // }
+    // else if(side1===side2||side2===side3||side1===side3){
+    //     alert ("Isoseles Triangle");
+    // }
+    // else if(side1!=side2 && side2!=side3 && side1!=side3){
+    //     alert ("Scalene Triangle");
+    // }
+    // else{
+    //     alert("Not a triangle"); 
+    // }
     
 }
